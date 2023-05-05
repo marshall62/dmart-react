acb8c92716034aa9bc535700b60f3b2e1199cb50
David Marshall <david.marshall@wwt.com>
2023-05-05 15:16:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
+import * as Realm from "realm-web";
 
 function App() {
+  // let artworks = null;
+  // let config = {};
+  // let realmApp = null;
+  
+  const homeArtworkRef = useRef({});
+  const configRef = useRef({});
+
+  
+
+
+  useEffect (() => {
+    const login =  async () => {    
+      const id = "dm-art-api-jznsb"; 
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        let realmApp = new Realm.App({id,});
+        const user = await realmApp.logIn(credentials);
+        console.log("Successfully logged in!", user.id);
+        const mongodb = realmApp.currentUser.mongoClient("mongodb-atlas");
+        let artwork = await mongodb.db("artworks").collection("works").findOne({tags: "home"});
+        homeArtworkRef.current = artwork;
+        const cfg = await mongodb.db("artworks").collection("config").findOne({});
+        configRef.current = cfg;
+        console.log(`Config`, cfg);
+        console.log(`Artwork `, artwork);
+      } catch (err) {
+        console.error("Failed to log in", err.message);     
+      }
+    }
+    
+    login();
+
+  });
+
+  const url = `${configRef.current.imageRootURI}/${homeArtworkRef.current.imagePath}`;
+  console.log("URL ", url);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="home" src={url}/>
+        {/* <img alt="home" src="https://raw.githubusercontent.com/marshall62/dmart/main/images/david_marshall_2.jpg"/> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello there
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
