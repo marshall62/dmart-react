fe474ee36d3a8b4fa96c44a3baea1a0a6ebd57ac
David Marshall <david.marshall@wwt.com>
2023-07-24 10:13:40 -0400

    Moved the caption of the main image below it.

--- src/galleryViews/MyLightbox.jsx
+++ src/galleryViews/MyLightbox.jsx
@@ -3,31 +3,38 @@ import ImageGallery from 'react-image-gallery';
 import { useContext, useState, useEffect } from "react";
 import { GlobalContext } from "../App";
 import "react-image-gallery/styles/css/image-gallery.css";
+import './my-lightbox.css';
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
 export default function MyLightbox ({artworks}) {
   const navigate = useNavigate();
-  const searchParams = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const global = useContext(GlobalContext);
 
-  const [artworkId, setArtworkId] = useState(searchParams.get('artwork'));
 
-  const images = artworks.map(aw => {
+  const [artworkId, setArtworkId] = useState(searchParams ? 
+    searchParams.get('artwork')
+    : '');
+
+  const images = global.artistConfig ? 
+    artworks.map(aw => {
     return {
       original: `${global.artistConfig.imageRootURI}/midsize/${aw.imagePath}`,
       fullscreen: `${global.artistConfig.imageRootURI}/${aw.imagePath}`,
       thumbnail: `${global.artistConfig.imageRootURI}/thumb/${aw.imagePath}`,
       description: `${aw.title}, ${aw.width} X ${aw.height}, ${aw.year}, ${aw.media}`
     }
-  });
+    }) :
+    [];
 
 
   useEffect(() => {
     const id = searchParams.get('artwork');
+    console.log("search params chg", searchParams);
+    console.log("id", id);
     setArtworkId(id);
   },[searchParams] )
 
-  console.log("search param",artworkId)
 
   const fn = (index) => {
     console.log("sliding to",artworks[index]._id.toString());
@@ -36,7 +43,7 @@ export default function MyLightbox ({artworks}) {
 
   return(
     <>
-      <ImageGallery onSlide={fn} showIndex={true} items={images}></ImageGallery>
+      <ImageGallery className="image-gallery-description" onSlide={fn} showIndex={true} items={images}></ImageGallery>
     </>
   )
 
